08b96e259eb75018987769f80e7208c135806716
Artemii Kurganov <aokurganov@mail.ru>
2023-09-08 21:42:45 +0500

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+Node JS 18.12.0
+
+Python 3.10.1
+
+`npm i`
+
+`node server.js`
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+var fs = require('fs');
+const { exec } = require("child_process");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/send", (req, res) => {
+    var jsonData = JSON.stringify(req.query);
+    fs.writeFile("data.json", jsonData, (err) => {
+        if (err)
+            console.log(err);
+    });
+    // exec('python ./main.py', runScript);
+    exec('python ./main.py', runScript);
+    res.sendStatus(200);
+})
+
+app.listen(3000);
+console.log("Listening on localhost:3000")
+
+const runScript = (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`${stdout}`);
+}
